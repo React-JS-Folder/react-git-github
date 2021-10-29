236a9678fba3f5f712c4918088b953425b931e9a
Marco <marco_trabajo@yahoo.com>
2021-10-29 21:47:05 +0200

    Tutorial 1 - video 5 - Basic Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,15 @@
-import Card from "./components/Card";
-
-function App() {
+export default function Card(){
   return (
     <>
-     {/* <img 
+     <img 
         style={{ width: '10rem' }}
         src="https://cdn.pixabay.com/photo/2017/02/15/12/12/cat-2068462_960_720.jpg" alt="animal" /> 
      <div>
        <p>Beau chat</p>
-       <p>Cats</p>
        <a href="/cats">More information...</a>
-     </div> */}
-     <Card />
-     <Card />
+     </div>
+    </>
+  )
 
-    </ >
-  );
 }
 
-export default App;
