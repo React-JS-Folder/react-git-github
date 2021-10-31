5f0f01bd346a95391b92e02aaae1b3c98b5fe5c6
Marco <marco_trabajo@yahoo.com>
2021-10-31 22:47:04 +0100

    Tutorial 7 - video 9 - Forms

--- src/App09.jsx
+++ src/App09.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import AppForm from './AppForm1oneInput'
-import AppForm2multipleInputs from './AppForm2multipleInputs'
-
+import Form01basicOneInput from './Form01basicOneInput'
+import Form02multipleInput from './Form02multipleInput'
+import Form03onSubmit from './Form03onSubmit'
+import Form04onSubmitResetFormData from './Form04onSubmitResetFormData'
+                                   
 export default function App09() {
   return (
     <div>
-
-        <AppForm />
-        <hr />
-        {/* <AppForm2multipleInputs /> */}
-        {/* <hr /> */}
-
+      <h4>this is form 1</h4>
+      <Form01basicOneInput />
+      <hr />
+      <h4>this is form 2</h4>
+      <Form02multipleInput />
+      <hr />
+      <h4>this is form 3</h4>
+      <Form03onSubmit />
+      <hr />
+      <h4>this is form 4</h4>
+      <Form04onSubmitResetFormData />
     </div>
   )
 }
